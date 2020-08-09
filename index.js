84ba044264a052ed45ba6dd1fbd4036d30c16fd1
llccing <lcf33123@foxmail.com>
2020-08-09 15:51:07 +0800

    update add session

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser')
 var formidable = require('formidable')
+var cookieParser = require('cookie-parser')
+var expressSession = require('express-session')
+// 使用文件系统存储 session
+var FileStore = require('session-file-store')(expressSession)
 
 var { getOneSentence, tours } = require('./src/home.logic');
+var { cookieSecret } = require('./src/credentials');
+const credentials = require('./src/credentials');
 
 var app = express();
 
@@ -21,6 +27,16 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 // 处理 json 格式的提交
 app.use(bodyParser.json())
+// app.use(cookieParser(cookieSecret))
+var fileStoreOptions = {}
+app.use(expressSession({
+  secret: cookieSecret,
+  store: new FileStore(fileStoreOptions),
+  cookie: {
+    maxAge: 1000 * 60 * 3
+  }
+}))
+
 
 app.set('port', process.env.PORT || 3000);
 
@@ -28,6 +44,7 @@ app.set('port', process.env.PORT || 3000);
 // app.VERB 帮我们做了很多工作，忽略了大小写和反斜杠
 // 路由支持通配符
 app.get('/', function (req, res) {
+  console.log(fileStoreOptions)
   res.render('home');
 })
 
@@ -38,7 +55,9 @@ app.get('/about', function (req, res) {
 })
 
 app.get('/form', function (req, res) {
-  res.render('form', { type: 'img', year: '2020', month: '08' })
+  console.log('/form', req.session)
+  const { username } = req.session
+  res.render('form', { type: 'img', year: '2020', month: '08', username })
 })
 
 app.get('/thank-you', function (req, res) {
@@ -48,8 +67,28 @@ app.get('/thank-you', function (req, res) {
 // 处理类似登录的 formData 信息
 app.post('/process-concact', function (req, res) {
   const { pass, name } = req.body
-  console.log(`pass=`, pass)
-  console.log(`name=`, name)
+
+  // 设置 cookie
+  res.cookie('monster', 'nom nom');
+  /**
+   * 设置签名cookie
+   * 使用 req.signedCookies 接收
+   * 被篡改的签名 cookie 会被服务器拒绝，并且 cookie 值会被重置为原始值。
+   */
+  res.cookie('sign_monster', 'nom nom', { signed: true });
+
+  res.render('form', { name, pass })
+})
+
+app.post('/login', function (req, res) {
+  const { pass, name } = req.body
+  req.session.username = name
+  req.session.colorScheme = req.session.colorScheme || 'dark'
+  console.log(req.session)
+  res.locals.flash = {
+    name,
+    message: '成功登录了'
+  }
   res.render('form', { name, pass })
 })
 
@@ -70,6 +109,12 @@ app.post('/upload/:type/:year/:month', function (req, res, next) {
 
 app.get('/api/tours', function (req, res) {
   res.json(tours)
+})
+
+app.use(function (req, res, next) {
+  res.locals.flash = req.session.flash;
+  // delete req.session.flash;
+  next()
 })
 
 // 定义了 404 页面
---
+++ src/credentials.js
@@ -0,0 +1,4 @@
+// 凭证外化，这个实际项目中需要放到 .gitignore 文件中做忽略，这里我们仅仅做演示用
+module.exports = {
+  cookieSecret: 'yourCookieSecret'
+}
